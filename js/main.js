e262fa86ea730f226fbf30a3eb9f893f187e8b41
michneaionuc <58982446+michneaionuc@users.noreply.github.com>
2024-11-11 12:26:15 +0200

    Implemented operations and calculate function

---
+++ js/main.js
@@ -0,0 +1,3 @@
+import { calculate } from './operations.js';
+
+console.log(calculate('-', 1, 2));
---
+++ js/operations.js
@@ -0,0 +1,29 @@
+const add = (firstNumber, secondNumber) => firstNumber + secondNumber;
+
+const substract = (firstNumber, secondNumber) => firstNumber - secondNumber;
+
+const multiply = (firstNumber, secondNumber) => firstNumber * secondNumber;
+
+const divide = (firstNumber, secondNumber) => {
+  if (secondNumber === 0) {
+    throw new Error('Cannot divide by zero');
+  }
+
+  return firstNumber / secondNumber;
+};
+
+export function calculate(operator, firstNumber, secondNumber) {
+  switch (operator) {
+    case '+':
+      return add(firstNumber, secondNumber);
+    case '-':
+      return substract(firstNumber, secondNumber);
+    case '*':
+      return multiply(firstNumber, secondNumber);
+    case '/':
+      return divide(firstNumber, secondNumber);
+
+    default:
+      throw new Error('Invalid operator');
+  }
+}
