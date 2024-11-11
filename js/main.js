1d55fa350805a78a9e75d559aeb624ea6556347f
Andrej Milanović <amilanovi0@gmail.com>
2024-11-11 14:17:44 +0100

    Add event listeners for calculator buttons

--- js/main.js
+++ js/main.js
@@ -1,3 +1,17 @@
 import { calculate } from './operations.js';
 
-console.log(calculate('-', 1, 2));
+const buttons = document.querySelectorAll('.calculator button');
+const display = document.getElementById('display');
+
+buttons.forEach((button) => {
+  button.addEventListener('click', () => {
+    const value = button.value;
+
+    // TODO: Add the rest of the buttons and their functionality
+    if (value === 'AC') {
+      console.log('AC');
+    } else if (value === 'C') {
+      console.log('C');
+    }
+  });
+});
